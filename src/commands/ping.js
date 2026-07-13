7538ab1de6615cecd9293a004af63f8392ace52d
FluxTroid <sagacrew2004@gmail.com>
2026-07-13 17:07:54 +0530

    Update ping.js

--- src/commands/ping.js
+++ src/commands/ping.js
@@ -1,1 +1,9 @@
-
+module.exports = {
+    name: 'ping',
+    category: 'General 🛠️', 
+    desc: 'Bot ගේ ක්‍රියාකාරීත්වය පරීක්ෂා කිරීම',
+    execute: async (sock, msg, args) => {
+        // බොට්ගෙන් යන රිප්ලයි එක
+        await sock.sendMessage(msg.key.remoteJid, { text: 'Pong! 🏓 Bot is active and running perfectly.' }, { quoted: msg });
+    }
+};
